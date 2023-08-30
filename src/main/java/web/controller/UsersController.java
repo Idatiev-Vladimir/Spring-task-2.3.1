200922a3a240d0c3e5e03834ce579be7ef2f9590
Vladimir <Idatiev08@gmail.com>
2023-08-31 00:00:17 +0300

    testing

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -12,6 +12,49 @@ public class UsersController {
 
 
 
+    private final UserService userService;
+
+    @Autowired
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String allUsers(Model model) {
+        model.addAttribute("Users", userService.allUser());
+        return "users";
+    }
+
+    @GetMapping("/new")
+    public String createUser(Model model) {
+        model.addAttribute("user", new User());
+        return "newUser";
+    }
+
+    @PostMapping("/")
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.addUser(user);
+        return "redirect:/";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.updateUser(user.getId(), user);
+        return "redirect:/";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editUser(@PathVariable("id") long id,
+                           Model model) {
+        model.addAttribute("user", userService.showUser(id));
+        return "editUser";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String removeUser(@PathVariable("id") long id) {
+        userService.removeUserById(id);
+        return "redirect:/";
+    }
 
 
 
